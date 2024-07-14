b36ff094e1717fb7a3945c1d664287bdb44c1020
rusla <rpavlov@ubic.tech>
2024-07-14 03:44:45 +0300

    initial commit

---
+++ src/test/java/rusio/pavlovsky/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package rusio.pavlovsky.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
